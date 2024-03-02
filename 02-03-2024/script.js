1d3a9ae1122983a522c50cb94df3087191537031
Nouman Fatta <81779387+NoumanFatta@users.noreply.github.com>
2024-03-02 15:14:30 +0500

    Add files via upload

---
+++ 02-03-2024/script.js
@@ -0,0 +1,85 @@
+var myname = "nouman";
+var profession = "developer";
+
+// alert("My name is " + myname);
+myname = "ahsan";
+// alert("My name is " + myname + " and my profession is " + profession); // concatenation
+
+// var myname = "Ahsan";
+// // myname = "Nouman";
+// alert(myname)
+
+// var myName = "nouman";
+// alert(myName);
+// var wight = 50;
+// var num1 = "1000";
+// var num2 = 100;
+// var num3 = 100;
+// var total = num1 + num2 + num3;
+var str1 = "Hello, ";
+var str2 = "World!";
+var myAlertName = "hello"; // camelCase
+var MyAlertName = "hello"; //PascalCase
+var my_alert_name = "hello"; // snake_case
+// alert(str1 + str2);
+// alert(22 % 2);
+// alert("My weigth is" + wight);
+
+var num = 5;
+num++; // num = num + 1
+num = num + 1;
+
+var num1 = 8;
+// num1 = 8
+var num2 = num1++; // num2 = 8 num1 = 9
+console.log(num1);
+console.log(num2);
+// alert(num1);
+// alert(num2);
+// alert(num2);
+
+// alert(num2++);
+// alert(num2);
+
+// var a = 9;
+// var b = ++a;
+// console.log("a = ", a);
+// console.log("b = ", b);
+
+var a = 2;
+var b = --a;
+console.log(a++);
+console.log(b);
+
+var x = 9;
+var y = x--;
+console.log("y=", y);
+console.log("x=", x);
+
+var totalCost = 1 + 3 * 4;
+var totalCost = (1 + 3) * 4;
+var totalCost = 2 * 4 * 4 + 2;
+var totalCost = 2 * 4 * 4 + 2;
+var totalCost = 8 * 6;
+
+console.log("totalCost=", totalCost);
+
+var str1 = "str1 ";
+var str2 = "str2";
+var result = str1 + "hello " + str2;
+console.log(result);
+
+var val1 = +prompt("Enter val 1");
+alert("You entered " + val1);
+var val2 = +prompt("Enter val 2");
+alert("You entered " + val2);
+var result = val1 + val2;
+alert("Sum of " + val1 + "+" + val2 + "=" + result);
+
+// BODMAS
+// B = Bracket
+// O = order
+// D = division
+// M = multiplication
+// A = addtion
+// S = subtraction
---
+++ 02-03-2024/script2.js
@@ -0,0 +1,18 @@
+// var mithai = "chamcham";
+
+var mithai = prompt("Enter mithai's name");
+
+if (mithai === "gulab jamun") {
+  alert("Le aao!");
+} else if (mithai === "chamcham") {
+  alert("aane do!");
+} else if (mithai === "barfi") {
+  alert("chalegi!");
+} else {
+  alert("rhne do!");
+}
+
+// 1. Ask user for val 1
+// 2. Ask user for operation ("+","-","*","/")
+// 3. Ask user for val 2
+// 4. print the resut 
