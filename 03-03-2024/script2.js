fb9f96a7907506300c86fa16e4042a74ad2b82e9
Nouman Fatta <81779387+NoumanFatta@users.noreply.github.com>
2024-03-03 15:17:32 +0500

    Add files via upload

---
+++ 03-03-2024/script2.js
@@ -0,0 +1,133 @@
+// var mithai = "chamcham";
+
+// var mithai = prompt("Enter mithai's name");
+// if (mithai === "gulab jamun") {
+//   alert("Le aao!");
+// } else if (mithai === "chamcham") {
+//   alert("aane do!");
+// } else if (mithai === "barfi") {
+//   alert("chalegi!");
+// } else {
+//   alert("rhne do!");
+// }
+
+// var a = 1;
+// if (a === 1) console.log("Right");
+// else console.log("Wrong");
+// console.log("hello");
+
+// 1. Ask user for val 1
+// 2. Ask user for operation ("+","-","*","/")
+// 3. Ask user for val 2
+// 4. print the resut
+
+// =================== CHAPTER 11=========================
+
+// var firstName = "Nouman";
+// var lastName = "Fatta";
+// var fullName = firstName + " " + lastName;
+// var x = 5;
+// var y = 10;
+// var totalCost = 50;
+
+// if (fullName === "Nouman Fatta") {
+//   console.log("pass", fullName);
+// }
+
+// if (totalCost === 25 * 2) {
+//   console.log("Right");
+// }
+// if (x * y === 3 * 5) {
+//   console.log("Right 2");
+// } else {
+//   console.log("wrong");
+// }
+
+// var x = 10;
+// if (x !== 8) {
+//   console.log("Right");
+// } else {
+//   console.log("Wrong");
+// }
+
+// if (7 >= 6) {
+//   console.log("true");
+// } else {
+//   console.log("false");
+// }
+
+// var a = 10;
+// if (a === 10) {
+//   console.log("Right");
+// }
+// if (a > 9) {
+//   console.log("right 2");
+// }
+// if (a > 10) {
+//   console.log("right 3");
+// } else {
+//   console.log("wrong 1");
+// }
+
+// =================== CHAPTER 13=========================
+
+var currentTime = 5;
+var timeLimit = 5;
+
+if (currentTime <= timeLimit) {
+  var age = +prompt("What is your age?");
+  if (age >= 18) {
+    var haveNIC = prompt("Do you have NIC?");
+    if (haveNIC === "YES") {
+      console.log("You can vote!");
+    } else {
+      console.log("Please bring NIC!");
+    }
+  } else {
+    console.log("You are not eligible!");
+  }
+} else {
+  console.log("time over!");
+}
+
+// var rice = "biryani";
+// var salan = "karahi";
+// var drink = "koi or";
+
+// var str = "string";
+// var num = 1;
+// var bool = true;
+
+// if (
+//   rice === "biryani" &&
+//   salan === "karahi" &&
+//   (drink === "cola" || drink === "pakola")
+// ) {
+//   console.log("Aane do!");
+// } else {
+//   console.log("rehne do!");
+// }
+
+// =================== CHAPTER 14=========================
+// var x = 5;
+// var y = 5;
+// var a = 1;
+// var b = 4;
+// var c = 8;
+// var d = 40;
+
+// if ((x === y || a === b) && c * 5 === d) {
+//   console.log("true");
+// } else {
+//   console.log("false");
+// }
+
+// var perc = 59;
+
+// if (perc >= 80 && perc <= 100) {
+//   console.log("A+");
+// } else if (perc >= 70) {
+//   console.log("A");
+// } else if (perc >= 60) {
+//   console.log("B");
+// }
