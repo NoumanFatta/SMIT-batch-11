ea60c611ac93d56cec564208bb3749eaee1d480d
NoumanFatta <81779387+NoumanFatta@users.noreply.github.com>
2024-03-23 13:22:09 +0500

    25 - 27

---
+++ 23-03-2024/index.js
@@ -0,0 +1,51 @@
+// var firstName = "Nouman";
+// var firstChar = firstName.charAt(firstName.length - 1);
+// console.log(firstChar);
+
+// var star = "*";
+// for (let index = 0; index < 5; index++) {
+//   console.log(star);
+//   star += "\n*";
+// }
+
+// var text = "Imran Khan jail me hai. Imran Khan bohot famous hai";
+// text = text.replace(/Imran Khan/g, "Chairman PTI");
+// console.log(text);
+
+var scoreAvg = 1.5;
+var roundedValue = Math.round(scoreAvg);
+console.log(roundedValue);
+
+var num = 1.2;
+var ceil = Math.ceil(num);
+console.log(ceil);
+
+var num = 1.2;
+var floor = Math.floor(num);
+console.log(floor);
+
+var randomNumber = Math.random();
+console.log(randomNumber);
+
+var bigDecimal = Math.random();
+var improvedNum = bigDecimal * 6 + 1;
+var numberOfStars = Math.floor(improvedNum);
+console.log(numberOfStars);
+
+var undf = "ada";
+if (" " || null) {
+  console.log("true");
+} else {
+  console.log("false");
+}
+
+// false ====== undefined | null | 0 | "" | false | NaN
+
+// true =======  (1 to infinity) | string | array |
+
+var str = "1";
+if (str === 1) {
+  console.log(true);
+} else {
+  console.log(false);
+}
