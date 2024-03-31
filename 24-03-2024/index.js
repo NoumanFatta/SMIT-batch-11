40790b72e02343405fa96969ca6b6ac073a6949f
Abdullah <81779387+NoumanFatta@users.noreply.github.com>
2024-03-31 12:16:25 +0500

    24-03-2024

---
+++ 24-03-2024/index.js
@@ -0,0 +1,41 @@
+// var str = "12313123nknknkn12312";
+// var num = parseInt(str);
+// console.log(num)
+
+// var str = "123.8njn";
+// var num = parseFloat(str);
+// console.log(num);
+
+// var num = 20000;
+// var str = num.toString();
+// console.log(str)
+
+// var num = 1.43255;
+// var str = num.toString();
+// if (str.charAt(str.length - 1) === "5") {
+//   str = str.slice(0, str.length - 1) + "6";
+// }
+// num = Number(str);
+// var prettyNum = num.toFixed(4);
+// console.log(prettyNum)
+
+// var rightNow = new Date();
+// console.log(rightNow);
+
+// var today = new Date();
+// var future = new Date("March 26, 2024 12:59:00");
+// var msToday = today.getTime();
+// var msfuture = future.getTime();
+
+// var msDiff = msfuture - msToday;
+// var seconds = msDiff / 1000;
+// var minutes = seconds / 60;
+// var hrs = minutes / 60;
+// var days = hrs / 24;
+// console.log(days);
+
+// var today = new Date();
+// today.setFullYear(2025);
+// today.setMonth(1)
+// today.setDate(29)
+// console.log(today);
