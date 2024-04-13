0470306951b4c21259b7218b5488fc61587005e0
Abdullah <81779387+NoumanFatta@users.noreply.github.com>
2024-04-13 14:54:22 +0500

    13-04-2024

---
+++ 13-04-2024/app.js
@@ -0,0 +1,105 @@
+// function greetUser(firstName, lastName) {
+//   console.log("Welcome to SMIT! " + firstName + " " + lastName);
+// }
+// var isLoggedin = true;
+// if (isLoggedin) {
+//   greetUser("Hassan", "Ahmed");
+// }
+// greetUser("Adil", "Affan");
+
+// function add(num1, num2) {
+//   var result = num1 + num2;
+//   return result;
+// }
+// var sum = add(5, 6);
+// console.log(sum);
+// console.log(add(7, 6));
+// console.log(add(7, 6));
+
+// function calculator(num1 = 12, num2 = 15, operator = "+") {
+//   if (operator === "+") {
+//     return num1 + num2;
+//   } else if (operator === "-") {
+//     return num1 - num2;
+//   } else if (operator === "*") {
+//     return num1 * num2;
+//   } else if (operator === "/") {
+//     return num1 / num2;
+//   } else {
+//     return "Inavlid operator";
+//   }
+// }
+// var num1 = undefined;
+// var result = calculator(num1);
+// console.log(result);
+
+// var theSum = 12;
+// function sum(num) {
+//   var theSum = num;
+//   console.log(theSum, "========");
+// }
+// sum(16);
+// console.log(theSum);
+// sum(22);
+// console.log(theSum);
+
+// var dayOfWk = "Mon";
+
+// switch (dayOfWk) {
+//   case "Sat":
+//     console.log("-=======");
+//     break;
+//   case "Mon":
+//   case "Sun":
+//     console.log("Whoopee");
+//     break;
+//   case "Fri":
+//     console.log("TGIF!");
+//     break;
+//   default:
+//     console.log("Shoot me now!");
+// }
+
+// var i = 0;
+// while (true) {
+//   console.log(i);
+//   i++;
+//   if (i === 3) {
+//     break;
+//   }
+// }
+
+// var i = 0;
+// do {
+//   console.log(i);
+//   i++;
+// } while (i < 3);
+// var i = 0;
+// do {
+//   console.log(i);
+//   i++;
+// } while (i < 0);
+
+// var i = 0;
+// function greet() {
+//   i++;
+//   console.log("hello user");
+//   if (i < 2) {
+//     greet();
+//   }
+// }
+
+// greet();
+
+// let n = 3;
+// function factorial(n) {
+//   if (n === 0) {
+//     return 1;
+//   } else {
+//     return n * factorial(n - 1);
+//   }
+// }
+
+// console.log(factorial(n));
+
+
