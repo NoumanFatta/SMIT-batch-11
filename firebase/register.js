563a16a7bf4fa87978fa24f0b4f56ab13621d11c
Nouman Fatta <noumanfatta@gmail.com>
2024-08-11 15:05:04 +0500

    firebase authentication

--- firebase/register.js
+++ firebase/register.js
@@ -1,29 +1,32 @@
 import {
   getAuth,
   createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  sendSignInLinkToEmail,
 } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-auth.js";
 import { app } from "./firebase-init.js";
 
 const auth = getAuth(app);
 
-const email = "email@test.com";
-const password = "123456";
-
-
-createUserWithEmailAndPassword(auth, email, password)
-  .then((userCredential) => {
-    // Signed up
-    // const user = userCredential.user;
-    // console.log(userCredential)
-    // console.log(user)
-    // ...
-  })
-  .catch((error) => {
-    // const errorCode = error.code;
-    // const errorMessage = error.message;
-    console.log(error)
-    if(error.code === "auth/email-already-in-use"){
-      console.log("Email already exits")
-    }
-    // ..
-  });
+const submitHandler = async (ev) => {
+  const actionCodeSettings = {
+    // URL you want to redirect back to. The domain (www.example.com) for this
+    // URL must be in the authorized domains list in the Firebase Console.
+    url: "http://localhost:5500/firebase/verify.html",
+    // This must be true.
+    handleCodeInApp: true,
+  };
+  try {
+    ev.preventDefault();
+    const form = new FormData(ev.currentTarget);
+    const email = form.get("email");
+    const password = form.get("password");
+    // const result = await createUserWithEmailAndPassword(auth, email, password);
+    const result = await sendSignInLinkToEmail(auth, email, actionCodeSettings);
+    
+    console.log(result);
+  } catch (error) {
+    console.log(error);
+  }
+};
+document.querySelector("form").addEventListener("submit", submitHandler);
