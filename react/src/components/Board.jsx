ec6a5d6a5fec6e2a92b993d77fafa5f45ad23ea3
Nouman Fatta <noumanfatta@gmail.com>
2024-09-15 14:57:51 +0500

    tic-tac-toe new logic

--- react/src/components/Board.jsx
+++ react/src/components/Board.jsx
@@ -4,22 +4,28 @@ import Square from "./Square";
 const Board = () => {
   const [turn, setTurn] = useState("first");
 
+  const [squares, setSquares] = useState(Array(9).fill(null));
+  const handleClick = (i) => {
+    const newSquare = [...squares];
+    newSquare[i] = "X";
+    setSquares(squares);
+  };
   return (
     <>
       <div className="board-row">
-        <Square turn={turn} setTurn={setTurn}/>
-        <Square turn={turn} setTurn={setTurn}/>
-        <Square turn={turn} setTurn={setTurn}/>
+        <Square value={squares[0]} handleClick={() => handleClick(0)} />
+        <Square value={squares[1]} handleClick={() => handleClick(1)} />
+        <Square value={squares[2]} handleClick={() => handleClick(2)} />
       </div>
       <div className="board-row">
-        <Square turn={turn} setTurn={setTurn}/>
-        <Square turn={turn} setTurn={setTurn}/>
-        <Square turn={turn} setTurn={setTurn}/>
+        <Square value={squares[3]} handleClick={() => handleClick(3)} />
+        <Square value={squares[4]} handleClick={() => handleClick(4)} />
+        <Square value={squares[5]} handleClick={() => handleClick(5)} />
       </div>
       <div className="board-row">
-        <Square turn={turn} setTurn={setTurn}/>
-        <Square turn={turn} setTurn={setTurn}/>
-        <Square turn={turn} setTurn={setTurn}/>
+        <Square value={squares[6]} handleClick={() => handleClick(6)} />
+        <Square value={squares[7]} handleClick={() => handleClick(7)} />
+        <Square value={squares[8]} handleClick={() => handleClick(8)} />
       </div>
     </>
   );
--- react/src/components/Square.jsx
+++ react/src/components/Square.jsx
@@ -1,23 +1,7 @@
 import { useState } from "react";
 
-const Square = ({ turn, setTurn }) => {
-  const [value, setValue] = useState(null);
-  console.log(turn);
-  const handleClick = () => {
-    if (value) return;
-    if (turn === "first") {
-      setValue("X");
-      setTurn("second");
-    } else {
-      setValue("O");
-      setTurn("first");
-    }
-  };
-  return (
-    <button className="square" onClick={handleClick}>
-      {value}
-    </button>
-  );
+const Square = ({ handleClick, value,}) => {
+  return <button className="square" onClick={handleClick}> {value} </button>;
 };
 
 export default Square;
