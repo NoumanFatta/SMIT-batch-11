bd25e43a66c15202644e2282dadb2de1c8312489
Nouman Fatta <noumanfatta@gmail.com>
2024-09-22 14:28:58 +0500

    use ref explained

--- react-concepts/src/components/Form.jsx
+++ react-concepts/src/components/Form.jsx
@@ -1,29 +1,37 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 
 const Form = () => {
+  const emailRef = useRef(null);
+  const myRefs= useRef([]);
   const [formDetails, setFormDetails] = useState({
     email: "test@test.com",
     password: "123",
   });
   const [isPwdVisible, setIsPwdVisible] = useState(false);
+
   const handleSumbit = (e) => {
     e.preventDefault();
+    console.log(emailRef.current.value);
     console.log(formDetails);
   };
+
   const onFormChange = (e) => {
     const { name, value } = e.target;
     setFormDetails((prev) => ({ ...prev, [name]: value }));
   };
+
   const togglePwdVis = () => {
     setIsPwdVisible(!isPwdVisible);
   };
+
   return (
     <form onSubmit={handleSumbit}>
       <input
+        ref={emailRef}
         type="text"
         name="email"
-        value={formDetails.email}
-        onChange={(e) => onFormChange(e)}
+        // value={formDetails.email}
+        // onChange={(e) => onFormChange(e)}
       />
       <input
         type={isPwdVisible ? "text" : "password"}
