aaf7846db2e2f510eb913d57b0e5038c4591509b
Nouman Fatta <noumanfatta@gmail.com>
2024-10-26 14:26:41 +0500

    usecallback and memo

--- todo-react/src/components/TodoComp.jsx
+++ todo-react/src/components/TodoComp.jsx
@@ -4,39 +4,22 @@ import Todos from "./Todos";
 const TodoComp = () => {
   const [count, setCount] = useState(0);
   const [todos, setTodos] = useState([]);
-  const expensiveCalculation = (num) => {
-    console.log("Calculating...");
-    for (let i = 0; i < 1000000000; i++) {
-      num += 1;
-    }
-    return num;
-  };
-  const calculation = useMemo(() => expensiveCalculation(count), [count]);
 
   const increment = () => {
     setCount((c) => c + 1);
   };
-  const addTodo = () => {
+  const addTodo = useCallback(() => {
     setTodos((t) => [...t, "New Todo"]);
-  };
+  }, []);
 
   return (
     <>
+      <Todos todos={todos} addTodo={addTodo} />
+
+      <hr />
       <div>
-        <div>
-          <h2>My Todos</h2>
-          {todos.map((todo, index) => {
-            return <p key={index}>{todo}</p>;
-          })}
-          <button onClick={addTodo}>Add Todo</button>
-        </div>
-        <hr />
-        <div>
-          Count: {count}
-          <button onClick={increment}>+</button>
-          <h2>Expensive Calculation</h2>
-          {calculation}
-        </div>
+        Count: {count}
+        <button onClick={increment}>+</button>
       </div>
     </>
   );
