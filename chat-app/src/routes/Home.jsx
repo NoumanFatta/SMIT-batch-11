a264c9e2c98840813bd06fe32d949494f38cbcbb
Nouman Fatta <noumanfatta@gmail.com>
2024-11-23 14:32:43 +0500

    cloudinary

--- chat-app/src/routes/Home.jsx
+++ chat-app/src/routes/Home.jsx
@@ -1,24 +1,56 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { useDispatch } from "react-redux";
 import { reject, success } from "../slices/dataSlice";
+const uploadToCloudinary = async (imageFile) => {
+  console.log(imageFile)
+  try {
+    const cloudName = "dvp2vsjy4";
+    const uploadPreset = "test_chatapp";
+
+    const formData = new FormData();
+    formData.append("file", imageFile);
+    formData.append("upload_preset", uploadPreset);
+
+    const response = await axios.post(
+      `https://api.cloudinary.com/v1_1/${cloudName}/image/upload`,
+      formData
+    );
+
+    const data = await response.json();
+    console.log(data);
+    return data.secure_url;
+  } catch (error) {
+    console.log("Errr=====", error);
+  }
+};
 
 const Home = () => {
   const dispatch = useDispatch();
-  useEffect(() => {
-    (async () => {
-      try {
-        const response = await axios.get(
-          "https://jsonplaceholder.typicode.com/todo/1"
-        );
-        dispatch(success(response.data));
-      } catch (error) {
-        dispatch(reject());
-        console.log("Err", error);
-      }
-    })();
-  }, []);
-  return <div>Home</div>;
+  const [file, setFile] = useState(null);
+
+  // useEffect(() => {
+  //   (async () => {
+  //     try {
+  //       const response = await axios.get(
+  //         "https://jsonplaceholder.typicode.com/todo/1"
+  //       );
+  //       dispatch(success(response.data));
+  //     } catch (error) {
+  //       dispatch(reject());
+  //       console.log("Err", error);
+  //     }
+  //   })();
+  // }, []);
+
+  return (
+    <>
+      <input type="file" onChange={(e) => {
+        setFile(e.target.files[0])
+      }} />
+      <button onClick={() => uploadToCloudinary(file)}>Home</button>;
+    </>
+  );
 };
 
 export default Home;
--- index.js
+++
@@ -1,8 +0,0 @@
-for (let i = 0; i < 10; i++) {
-
-    
-  if (i === 5 || i == 7) {
-    continue;
-  }
-  console.log(i);
-}
